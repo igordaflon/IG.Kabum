c00d3a78dabf65f47a4aa88d904453f3cd03e952
Igor Couto <igor.couto@autoglass.com.br>
2023-09-02 21:32:04 -0300

    feat: Alteração WebApp

---
+++ src/KBM.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KBM.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
