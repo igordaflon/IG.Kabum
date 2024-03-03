418c24a15103f99008787a802bd61d8d14ece1cc
Igor Daflon do Couto <igordafloncouto@gmail.com>
2024-03-03 16:57:32 -0300

    Cria classe AspNetUser

--- src/Web/KBM.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/Web/KBM.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,4 +1,5 @@
-﻿using KBM.WebApp.MVC.Services;
+﻿using KBM.WebApp.MVC.Extensions;
+using KBM.WebApp.MVC.Services;
 
 namespace KBM.WebApp.MVC.Configuration;
 
@@ -7,5 +8,8 @@ public static class DependencyInjectionConfig
     public static void RegisterServices(this IServiceCollection services)
     {
         services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
+
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        services.AddScoped<IUser, AspNetUser>();
     }
 }
