d20f5632f7ccc8f3072d6db4628304550727db23
Igor Couto <igordafloncouto@gmail.com>
2024-04-13 22:27:14 -0300

    Cria controller catalogo

---
+++ src/Servicos/KBM.Catalogo.API/Controllers/CatalogoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KBM.Catalogo.API.Controllers
+{
+    public class CatalogoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
